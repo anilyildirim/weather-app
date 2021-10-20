70944d0b9a323040d3ec46f0c26422d656868b31
anil <anilyildirim3pc@gmail.com>
2021-10-20 09:22:28 +0200

    fix: use correct semantic elements

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -52,49 +52,49 @@ function App() {
   }
 
   return (
-    <div className={
+    <main className={
       (typeof weather.main != "undefined")
        ? ((weather.main.temp > 16)
         ? 'app warm' 
         : 'app')
       : 'app'}>
-      <main>
-        <div className="search-box">
-          <form 
-            as="form"
-            role="search"
-            action="/"
-            method="get"
-            onSubmit={search}
-          >
-            <input 
-              type="text" 
-              className="search-bar" 
-              placeholder="search" 
-              onChange={ e => setQuery(e.target.value)}
-              value={query}
-              onKeyPress={(e) => (
-                (e.key === 'Enter') ? search(e): '')}
-            />
-            <button type="submit">submit</button>
-            <button type="reset" onClick={(e) => setQuery(e.target.value)}>reset</button>
-          </form>
-        </div>
+      <div className="app__inner">
+        <form 
+          as="form"
+          role="search"
+          action="/"
+          className="search-form"
+          method="get"
+          onSubmit={search}
+        >
+          <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="search" 
+            onChange={ e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={(e) => (
+              (e.key === 'Enter') ? search(e): '')}
+          />
+          <button type="submit">submit</button>
+          <button type="reset" onClick={(e) => setQuery(e.target.value)}>reset</button>
+        </form>
         {( typeof weather.main !== "undefined") ? (
           <div>
             <div className="searched-city">
               <div className="location-box">
-                <div className="location">
+                <p className="location">
                   {weather.name}, {weather.sys.country}
-                </div>
-                <div className="date">{dateBuilder(new Date())}</div>
+                </p>
+                {/* TODO:Fix datetime attribute */}
+                <time datetime="2008-02-14" className="date">{dateBuilder(new Date())}</time>
               </div>
               <div className="weather-box">
-                <div className="temp">{Math.round(weather.main.temp)} °C</div>
-                <div className="weather">{weather.weather[0].main}</div>
+                <p className="temp">{Math.round(weather.main.temp)} °C</p>
+                <p className="weather">{weather.weather[0].main}</p>
               </div>
             </div>
-            <div className="suggested-cities">
+            {/* <div className="suggested-cities">
               {suggestions && suggestions.map((suggestion) => (
                 <div className="weather-box suggested-city" key={ suggestion.id }>
                   <span>{suggestion.name}</span>
@@ -102,16 +102,15 @@ function App() {
                   <div className="weather">{suggestion.weather[0].main}</div>
                 </div>
               ))}
-            </div>
+            </div> */}
           </div>
-          
         ) : (query) ? (
           <div>
-            <p className="error-warning">We couldn't find any results for {query}. Here is the error code: <span>{weather.message}!</span></p>
+            <strong className="error-warning">We couldn't find any results for {query}. Here is the error code: <span>{weather.message}!</span></strong>
           </div>
           ) : ''}
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
 
